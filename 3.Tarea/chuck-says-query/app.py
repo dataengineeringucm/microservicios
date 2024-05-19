b703a1688dead65a6fa2a0f0cf50e3bdefeada67
Oscar Gomez Soriano <ogomezso0@gmail.com>
2024-05-19 14:49:07 +0100

    query by id and requirements.txt fixed

--- 3.Tarea/chuck-says-query/app.py
+++ 3.Tarea/chuck-says-query/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify, request
-from pymongo import MongoClient
-from bson.objectid import ObjectId
 import configparser
+
+from flask import Flask, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
@@ -11,6 +11,7 @@ mongo_uri = config.get('DatabaseSection', 'mongo.uri')
 client = MongoClient(mongo_uri)
 db = client.test
 
+
 @app.route('/facts', methods=['GET'])
 def get_facts():
   facts = list(db.facts.find())
@@ -19,12 +20,16 @@ def get_facts():
   print(facts)
   return jsonify(facts)
 
+
 @app.route('/facts/<id>', methods=['GET'])
 def get_fact(id):
-  fact = db.facts.find_one({'_id': ObjectId(id)})
-  fact['_id'] = str(fact['_id'])
-  print(fact)
-  return jsonify(fact)
+  fact = db.facts.find_one({'id': int(id)})
+  if fact:
+    fact['_id'] = str(fact['_id'])
+    print(fact)
+    return jsonify(fact)
+  else:
+    return jsonify({"error": "Fact not found"}), 404
 
-if  __ame__ == '__main__':
+if __name__ == '__main__':
   app.run(host='0.0.0.0', port=int(config.get('ServerSection', 'server.port')))
